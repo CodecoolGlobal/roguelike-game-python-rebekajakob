ba395bb45a519afa4d1ca36bfc4d3273ec4052c1
Botond <63496999+B-T-ND@users.noreply.github.com>
2022-04-25 13:01:28 +0200

    see board size

--- testing.py
+++ testing.py
@@ -1,12 +1,26 @@
-#coloring
+# coloring
 import colorama
 
-a = colorama.Fore.CYAN + "Q"
-print({a})
+# a = colorama.Fore.CYAN + "Q"
+# print({a})
 
-#get keypress
 import util
-key = ""
-while key != 'q':
-    key = util.key_pressed()
-    print({key})
+def get_keypress():
+    key = ""
+    while key != 'q':
+        key = util.key_pressed()
+        print(key)
+        print({key})
+# get_keypress()
+
+# board size
+def print_board(width=30, height=20):
+    board = []
+    for i in range(height):
+        row = []
+        for i in range(width):
+            row.append("X")
+        board.append(row)
+    for row in board:
+        print(' '.join(row))
+print_board()
