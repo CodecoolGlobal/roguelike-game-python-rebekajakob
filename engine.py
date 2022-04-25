31da14c3ca662e28b755ed58eb7761347b73e580
rebekajakob <rebusacka@gmail.com>
2022-04-25 14:39:04 +0200

    we have walls

--- engine.py
+++ engine.py
@@ -1,4 +1,4 @@
-def create_room(entry_door, exit_door, width=30, height=20):
+def create_room(entry_door, exit_door,level, width=30, height=20):
     board = []
     for i in range(height):
         row = []
@@ -12,6 +12,30 @@ def create_room(entry_door, exit_door, width=30, height=20):
                     row.append(0)
         board.append(row)
     create_doors(board, entry_door, exit_door)
+    if level == 1:
+        place_wall(board, (10,0), (10,10))
+        place_wall(board, (5,15), (13,15))
+        place_wall(board, (15,5), (19,5))
+        place_wall(board, (15,15), (19,15))
+        place_wall(board, (7,19), (7,25))
+        place_wall(board, (8,19), (8,25))
+        place_wall(board, (9,19), (9,25))
+        place_wall(board, (10,19), (10,25))
+        place_wall(board, (11,19), (11,25))
+    if level == 2:
+        place_wall(board, (0,8), (15,8))
+        place_wall(board, (10,14), (19,14))
+        place_wall(board, (10,15), (19,15))
+        place_wall(board, (0,18), (15,18))
+        place_wall(board, (7,21), (7,25))
+        place_wall(board, (12,21), (12,29))
+        place_wall(board, (0,25), (7,25))
+    if level == 3:
+        place_wall(board, (5, 10), (5, 10))
+        place_wall(board, (15, 11), (15, 11))
+        place_wall(board, (4, 20), (4, 20))
+        place_wall(board, (10, 20), (10, 20))
+        
     return board
 
 
@@ -21,6 +45,15 @@ def create_doors(board: list, entry_door: tuple, exit_door: tuple):
         board[entry_door[0]][entry_door[1]] = 2
     if exit_door:
         board[exit_door[0]][exit_door[1]] = 3
+
+def place_wall(board, start_wall, end_wall):
+    if start_wall[0] == end_wall[0]:
+        for i in range(start_wall[1],end_wall[1]+1):
+            board[start_wall[0]][i] = 1
+    else:
+        for i in range(start_wall[0],end_wall[0]+1):
+            board[i][start_wall[1]] = 1
+        
     
 
 
@@ -41,10 +74,13 @@ def put_player_on_board(board, player):
 
 
 if __name__ == "__main__":
-    level_1_board = create_room(None, (0, 10))
-    for i in level_1_board:
+    room1 = create_room(None, (4, 29),1)
+    for i in room1:
         print(" ".join(map(str, i)))
-    level_2_board = create_room((19, 10), None)
-    for i in level_2_board:
+    room2 = create_room((5, 0),(19, 26),2)
+    for i in room2:
+        print(" ".join(map(str, i)))
+    room3 = create_room((0,26),None,3)
+    for i in room3:
         print(" ".join(map(str, i)))
     
