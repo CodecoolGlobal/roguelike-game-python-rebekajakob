de31b0fcb3fa971db0254072bcdde37d77551cc2
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2022-04-25 13:55:22 +0200

    Board development

--- engine.py
+++ engine.py
@@ -1,15 +1,29 @@
-def create_board(width, height):
-    '''
-    Creates a new game board based on input parameters.
+def create_room(entry_door, exit_door, width=30, height=20):
+    board = []
+    for i in range(height):
+        row = []
+        for col in range(width):
+            if i == 0 or i == height - 1:
+                row.append(1)
+            else:
+                if col == 0 or col == width - 1:
+                    row.append(1)
+                else:
+                    row.append(0)
+        board.append(row)
+    create_doors(board, entry_door, exit_door)
+    return board
 
-    Args:
-    int: The width of the board
-    int: The height of the board
 
-    Returns:
-    list: Game board
-    '''
-    pass
+    # create doors
+def create_doors(board: list, entry_door: tuple, exit_door: tuple):
+    if entry_door:
+        board[entry_door[0]][entry_door[1]] = 2
+    if exit_door:
+        board[exit_door[0]][exit_door[1]] = 3
+    
+
+
 
 
 def put_player_on_board(board, player):
@@ -24,3 +38,13 @@ def put_player_on_board(board, player):
     Nothing
     '''
     pass
+
+
+if __name__ == "__main__":
+    level_1_board = create_room(None, (0, 10))
+    for i in level_1_board:
+        print(" ".join(map(str, i)))
+    level_2_board = create_room((19, 10), None)
+    for i in level_2_board:
+        print(" ".join(map(str, i)))
+    
