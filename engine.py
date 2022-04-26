61636332dbc4189ea199a243761cafc2dacf90ed
Botond <63496999+B-T-ND@users.noreply.github.com>
2022-04-26 13:06:09 +0200

    added comments

--- engine.py
+++ engine.py
@@ -3,8 +3,11 @@ import util
 import main
 
 
-def create_room(entry_door, exit_door, level, width=30, height=20):
-    board = []
+def create_room(entry_door: tuple, exit_door: tuple, level: int, width=30, height=20):
+    """Generates the rooms for the game
+    (hard coded rooms)
+    """
+    room = []
     for row in range(height):
         current_row = []
         for col in range(width):
@@ -15,35 +18,38 @@ def create_room(entry_door, exit_door, level, width=30, height=20):
                     current_row.append(1)
                 else:
                     current_row.append(0)
-        board.append(current_row)
-    create_doors(board, entry_door, exit_door)
+        room.append(current_row)
+    create_doors(room, entry_door, exit_door)
     if level == 1:
-        place_inner_wall(board, (10, 0), (10, 10))
-        place_inner_wall(board, (5, 15), (13, 15))
-        place_inner_wall(board, (15, 5), (19, 5))
-        place_inner_wall(board, (15, 15), (19, 15))
-        place_inner_wall(board, (7, 19), (7, 25))
-        place_inner_wall(board, (8, 19), (8, 25))
-        place_inner_wall(board, (9, 19), (9, 25))
-        place_inner_wall(board, (10, 19), (10, 25))
-        place_inner_wall(board, (11, 19), (11, 25))
+        place_inner_wall(room, (10, 0), (10, 10))
+        place_inner_wall(room, (5, 15), (13, 15))
+        place_inner_wall(room, (15, 5), (19, 5))
+        place_inner_wall(room, (15, 15), (19, 15))
+        place_inner_wall(room, (7, 19), (7, 25))
+        place_inner_wall(room, (8, 19), (8, 25))
+        place_inner_wall(room, (9, 19), (9, 25))
+        place_inner_wall(room, (10, 19), (10, 25))
+        place_inner_wall(room, (11, 19), (11, 25))
     if level == 2:
-        place_inner_wall(board, (0, 8), (15, 8))
-        place_inner_wall(board, (10, 14), (19, 14))
-        place_inner_wall(board, (10, 15), (19, 15))
-        place_inner_wall(board, (0, 18), (15, 18))
-        place_inner_wall(board, (7, 21), (7, 25))
-        place_inner_wall(board, (12, 21), (12, 29))
-        place_inner_wall(board, (0, 25), (7, 25))
+        place_inner_wall(room, (0, 8), (15, 8))
+        place_inner_wall(room, (10, 14), (19, 14))
+        place_inner_wall(room, (10, 15), (19, 15))
+        place_inner_wall(room, (0, 18), (15, 18))
+        place_inner_wall(room, (7, 21), (7, 25))
+        place_inner_wall(room, (12, 21), (12, 29))
+        place_inner_wall(room, (0, 25), (7, 25))
     if level == 3:
-        place_inner_wall(board, (5, 10), (5, 10))
-        place_inner_wall(board, (15, 11), (15, 11))
-        place_inner_wall(board, (4, 20), (4, 20))
-        place_inner_wall(board, (10, 20), (10, 20))
-    return board
+        place_inner_wall(room, (5, 10), (5, 10))
+        place_inner_wall(room, (15, 11), (15, 11))
+        place_inner_wall(room, (4, 20), (4, 20))
+        place_inner_wall(room, (10, 20), (10, 20))
+    return room
 
 
 def create_board():
+    """Puts the rooms together into one board list
+    (rooms need to be added manually)
+    """
     board = []
     board.append(create_room(None, (4, 29), 1))
     board.append(create_room((4, 0), (19, 26), 2))
@@ -51,14 +57,19 @@ def create_board():
     return board
 
 
-def create_doors(board: list, entry_door: tuple, exit_door: tuple) -> None:
+def create_doors(room: list, entry_door: tuple, exit_door: tuple) -> None:
+    """Adds doors to given room
+    write None to not add the door
+    """
     if entry_door:
-        board[entry_door[0]][entry_door[1]] = 2
+        room[entry_door[0]][entry_door[1]] = 2
     if exit_door:
-        board[exit_door[0]][exit_door[1]] = 3
+        room[exit_door[0]][exit_door[1]] = 3
 
 
 def place_inner_wall(board, start_wall, end_wall):
+    """Helper function for the room creation
+    """
     if start_wall[0] == end_wall[0]:
         for i in range(start_wall[1], end_wall[1]+1):
             board[start_wall[0]][i] = 1
@@ -68,15 +79,26 @@ def place_inner_wall(board, start_wall, end_wall):
 
 
 def put_player_on_board(room, player):
+    """puts player on given room,
+    needs the player stats
+    """
     room[player['X']][player['Y']] = 4
 
 
 def new_player_position(old_player_coordinates: tuple, direction: tuple) -> tuple:
+    """puts player 1 block in the direction
+    of the players current position"""
     new_player_coordinates = (old_player_coordinates[0] + direction[0], old_player_coordinates[1] + direction[1])
     return new_player_coordinates
 
 
 def check_target_cell(room, player_coordinates: tuple, direction: tuple):
+    """checks if the move to the given direction is valid
+    returns:
+    1 = valid move to empty cell
+    2 = entry door
+    3 = exit door
+    """
     potential_cell = new_player_position(player_coordinates, direction)
     if room[potential_cell[0]][potential_cell[1]] == 0:
         return 1
--- main.py
+++ main.py
@@ -13,6 +13,11 @@ color_scheme = {0: ' ', 1: '▅', 2: 'E', 3: 'X', 4: 'O'}
 
 
 def create_player():
+    """Sets the player's attributes
+    'X'=starter position
+    'Y'=starter position
+    'HP'= hitpoint
+    """
     player = {'X': PLAYER_START_X, 'Y': PLAYER_START_Y, 'HP': 100}
     return player
 
@@ -33,13 +38,13 @@ def main():
         direction_vectors = {'w': (-1, 0), 's': (1, 0), 'a': (0, -1), 'd': (0, 1)}
         if button in direction_vectors:
             direction = direction_vectors[button]
-            if engine.check_target_cell(current_room, player_coordinates, direction) == 1:
+            if engine.check_target_cell(current_room, player_coordinates, direction) == 1: # player made a move to en empty cell
                 current_room[player_coordinates[0]][player_coordinates[1]] = 0
                 player_coordinates = engine.new_player_position(player_coordinates, direction)
                 player['X'], player['Y'] = player_coordinates[0], player_coordinates[1]
                 current_room[player_coordinates[0]][player_coordinates[1]] = 4
                 
-            elif engine.check_target_cell(current_room, player_coordinates, direction) == 2:
+            elif engine.check_target_cell(current_room, player_coordinates, direction) == 2: # player went to the entry door of the room
                 current_room[player_coordinates[0]][player_coordinates[1]] = 0
                 current_room_index -= 1
                 current_room = board[current_room_index]
@@ -48,7 +53,7 @@ def main():
                 elif current_room_index == 1:
                     player['X'], player['Y'] = 18, 26
                    
-            elif engine.check_target_cell(current_room, player_coordinates, direction) == 3:
+            elif engine.check_target_cell(current_room, player_coordinates, direction) == 3: # player went to the exit door of the room
                 current_room[player_coordinates[0]][player_coordinates[1]] = 0
                 current_room_index += 1    
                 current_room = board[current_room_index]
@@ -58,7 +63,7 @@ def main():
                     player['X'], player['Y'] = 1, 26
         util.clear_screen()
 
-        
+
 
 
 
